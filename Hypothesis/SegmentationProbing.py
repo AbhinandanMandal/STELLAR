f0af0d7ad26f0bfbbf5d5216bf2236a2b808d6ca
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2026-09-07 14:26:16 +0530

    Segmentation Probing

---
+++ Hypothesis/SegmentationProbing.py
@@ -0,0 +1,11 @@
+
+
+""" 
+For segmentation probing, we've also done it in huggingface as, 
+
+from src.models.downstream.segmentation import SegmentationProbing
+"""
+
+
+def SegmentationProbing(model_backbone, is_baseline, feature_key, feature_dim, num_classes, freeze_backbone, freeze_model, resize_output):
+    return
